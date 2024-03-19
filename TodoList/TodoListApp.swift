b354ee3eca0429fb3075ab14db3f3410e6b00c32
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2024-03-20 02:29:35 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ahmet Kaçar on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
